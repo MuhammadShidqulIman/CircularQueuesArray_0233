1ce7cac1a77781bf8515b4c81d8aa7deb4cb2236
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-30 15:28:17 +0700

    Menambahkan logika tampilan untuk elemen-elemen antrian saat FRONT > REAR

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -84,6 +84,13 @@ public:
             }
             cout << endl;
         }
+        else {
+            // Jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1) {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+        }
     }
 };
 
