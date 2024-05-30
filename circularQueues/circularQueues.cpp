10a83c940061a936b4dec977e4725c38b4359b8c
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-30 14:59:59 +0700

    jika REAR berada di posisi terakhir array,kembali ke awal array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -31,6 +31,17 @@ public:
             REAR = 0;
         }
 
-  
+        else {
+            //jika REAR berada di posisi terakhir array,kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+
+
+    }
+};
 
 int main()
